3b548e6c1eb41f3d71343e82f129d434d0b95b05
wuyihang <2025151726@qq.com>
2021-08-19 17:26:54 +0800

    改成get了加法

--- src/views/plus.vue
+++ src/views/plus.vue
@@ -4,13 +4,14 @@
       <input type="text" placeholder="第一个数字" ref="first">
       <input type="text" placeholder="第二个数字" ref="second">
       <button @click="submitForm">提交</button>
-      <input type="text" ref="result">
+      <input type="text" ref="answer">
     </form>
   </div>
 </template>
 
 <script>
 import {postParams} from "../api/api";
+import axios from "axios";
 
 export default {
   name: "plus",
@@ -22,16 +23,26 @@ export default {
     submitForm:function (){
       console.log(this.$refs.first.value)
       console.log(this.$refs.second.value)
-      postParams(this.$refs.first.value,this.$refs.second.value).then((res)=>{
-        console.log(res.data)
-        this.$refs.result.value=res.data
+      // postParams(this.$refs.first.value,this.$refs.second.value).then((res)=>{
+      //   console.log(res.data)
+      //   this.$refs.result.value=res.data
+      // })
+      axios({
+        method:'get',
+        url:'http://101.200.150.87/plus',
+        params:{
+          first:this.$refs.first,
+          second:this.$refs.second
+        }
+      }).then(response =>{
+        this.$refs.answer=response.data.answer
       })
       // this.$refs.result.value='test';
-      console.log(this.$refs.result.value)
+      console.log(this.$refs.answer.value)
     }
   },
   mounted() {
-    this.$refs.result.value='尚未提交'
+    this.$refs.answer.value='尚未提交'
   }
 }
 </script>
