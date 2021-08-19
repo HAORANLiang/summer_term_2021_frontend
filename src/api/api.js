48c4783f2c48a9e16ad53545cf453b70cefb84c0
shiro641 <70186086+shiro641@users.noreply.github.com>
2021-08-19 17:31:05 +0800

    update

--- src/api/api.js
+++ src/api/api.js
@@ -4,4 +4,12 @@ import axiosInstance from './index'
 const axios = axiosInstance
 
 
-export const postNub = (first, second) => {return axios.post(`http://101.200.150.87/jian`, {'first': first, 'second': second})}
+export const postNub = (first, second) => {
+    return axios.get(`http://101.200.150.87/jian`, {
+        params: {
+            first: first,
+            second: second
+        }
+    }
+    )
+}
